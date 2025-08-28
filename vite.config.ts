1bffa61d90db8e1c27e83094ec24ddb6cbc78eeb
Thanh Nguyen <thanh.nguyen@go2joy.vn>
2025-08-28 16:28:33 +0700

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import vueDevTools from 'vite-plugin-vue-devtools'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [vue(), vueDevTools()],
+//   base: '/admin-cho-thue-tro-x1/', // 👈 quan trọng
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url)),
+//     },
+//   },
+// })
+
+// vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  base: '/admin-cho-thue-tro-x1/', // 👈 quan trọng
+  plugins: [vue()],
+  base: '/admin-cho-thue-tro-x1/', // 👈 tên repo
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 })
