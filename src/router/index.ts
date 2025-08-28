79d20048f365d8aef47e2e5e74943b91deccf4f2
Thanh Nguyen <thanh.nguyen@go2joy.vn>
2025-08-28 16:42:53 +0700

    Update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,46 @@
+// // export default router
+
+// import { createRouter, createWebHashHistory, type RouteRecordRaw } from 'vue-router'
+// import { h } from 'vue'
+// import DashboardList from '@/views/dashboard/list/View.vue'
+
+// // routerView dùng làm parent container
+// export const routerView = {
+//   render() {
+//     return h('router-view')
+//   },
+// }
+
+// // Routes cơ bản
+// const routes: RouteRecordRaw[] = [
+//   {
+//     path: '/',
+//     component: DashboardList,
+//   },
+// ]
+
+// const router = createRouter({
+//   history: createWebHashHistory(),
+//   routes,
+// })
+
 // export default router
 
-import { createRouter, createWebHashHistory, type RouteRecordRaw } from 'vue-router'
-import { h } from 'vue'
-import DashboardList from '@/views/dashboard/list/View.vue'
-
-// routerView dùng làm parent container
-export const routerView = {
-  render() {
-    return h('router-view')
-  },
-}
-
-// Routes cơ bản
-const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    component: DashboardList,
-  },
-]
+// router.ts
+import { createRouter, createWebHistory } from 'vue-router'
+import DefaultLayout from '../layouts/DefaultLayout.vue'
+import DashboardList from '../views/dashboard/list/View.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+  history: createWebHistory('/admin-cho-thue-tro-x1/'), // 👈 Quan trọng
+  routes: [
+    { path: '/', redirect: '/dashboard/list' },
+    {
+      path: '/dashboard',
+      component: DefaultLayout,
+      children: [{ path: 'list', component: DashboardList }],
+    },
+  ],
 })
 
 export default router
