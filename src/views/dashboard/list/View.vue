224cee0b81cc6bb166e081c4d29a6a5d7624cfa6
Thanh Nguyen <thanh.nguyen@go2joy.vn>
2025-08-28 16:25:14 +0700

    Update

--- src/views/dashboard/list/View.vue
+++ src/views/dashboard/list/View.vue
@@ -1,14 +1,9 @@
 <template>
-    <HaSection>
-        <div>Profile 2</div>
-    </HaSection>
+  <HaSection>
+    <div>Profile 2</div>
+  </HaSection>
 </template>
 
 <script lang="ts" setup>
 import HaSection from '@/components/global/HaSection.vue'
-import { onMounted } from 'vue'
-
-onMounted(() => {
-    console.log('Profile 2 component mounted')
-})
 </script>
