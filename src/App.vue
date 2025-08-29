6932566aadbad9b20896e473b8c29d88b8f2b828
Thanh Nguyen <thanh.nguyen@go2joy.vn>
2025-08-29 11:16:00 +0700

    Update

--- src/App.vue
+++ src/App.vue
@@ -19,16 +19,3 @@ const layoutComponent = computed(() => {
 <style lang="scss">
 @use './assets/scss/main.scss';
 </style>
-
-
-<!-- 
-<template>
-  <div>
-    <h1>Vue 3 SPA Demo</h1>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view></router-view>
-  </div>
-</template> -->
--- src/views/Home.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h2>Home Page</h2>
-    <p>Welcome to the demo SPA!</p>
-
-    <el-link>aaaaaaaaaaaaaaaaaa</el-link>
-  </div>
-</template>
