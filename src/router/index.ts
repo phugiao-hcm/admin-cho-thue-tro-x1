8a5f5ffd375e7abb039a6a27b8abff34a8d44386
Thanh Nguyen <thanh.nguyen@go2joy.vn>
2025-08-29 11:19:58 +0700

    Update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,53 +1,8 @@
 // // export default router
-
-// import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
-// import { h } from 'vue'
-
-// // routerView dùng làm parent container
-// export const routerView = {
-//   render() {
-//     return h('router-view')
-//   },
-// }
-
-// // Routes cơ bản
-// const routes: RouteRecordRaw[] = [
-//   { path: '/', redirect: `/dashboard/list` }, // default route
-// ]
-
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes,
-// })
-
-// export default router
-
-// import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
-// import Home from '../views/Home.vue'
-// import About from '../views/About.vue'
-
-// const routes = [
-//   { path: '/', component: Home },
-//   { path: '/about', component: About },
-// ]
-
-// const routes: RouteRecordRaw[] = [
-//   // { path: '/', redirect: `/dashboard/list` }, // default route
-//   { path: '/', component: Home },
-//   { path: '/about', component: About },
-//   { path: '/project/list', component: ProjectList },
-// ]
-
-// const router = createRouter({
-//   history: createWebHistory('/admin-cho-thue-tro-x1/'),
-//   routes,
-// })
-
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 
 // Base routes
 const baseRoutes: RouteRecordRaw[] = [
-  // { path: '/', component: Home },
   { path: '/', redirect: `/dashboard/list` }, // default route]
 ]
 
