190d21090864f031cbb5c5cf686a6e0d664127e0
thanhnguyen <thanh.nguyen@go2joy.vn>
2025-12-27 13:52:51 +0700

    Update

--- src/views/posts/list/View.vue
+++ src/views/posts/list/View.vue
@@ -1,119 +1,133 @@
 <template>
-  <HaSection>
-    <div class="flex-between" style="margin: 16px 0">
-      <p class="body-large-semi-bold neutral-700">Danh sách Tin đăng</p>
+    <HaSection>
+        <div class="flex-between" style="margin: 16px 0">
+            <p class="body-large-semi-bold neutral-700">Danh sách Tin đăng</p>
 
-      <div class="flex-center">
-        <el-select
-          v-model="filter.status"
-          placeholder="Select"
-          style="width: 240px"
-          @change="onSearch"
-        >
-          <el-option
-            v-for="item in statusOptions"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-          />
-        </el-select>
-        <!-- <div>
+            <div class="flex-center">
+                <el-select
+                    v-model="filter.status"
+                    placeholder="Select"
+                    style="width: 240px"
+                    @change="onSearch"
+                >
+                    <el-option
+                        v-for="item in statusOptions"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                    />
+                </el-select>
+                <!-- <div>
                     <el-input
                         v-model="filter.keyword"
                         style="width: 240px"
                         placeholder="Nhập tìm kiếm..."
                     />
                 </div> -->
-        <div>
-          <el-button type="primary" @click="onDirectProjectCreate">Tạo Tin đăng</el-button>
-        </div>
-      </div>
-    </div>
-
-    <div class="el-table-outer">
-      <el-table
-        v-loading="ui.isLoading"
-        :data="tableData"
-        :header-cell-style="{
-          background: '#F9FAFB',
-          borderBottom: '1px solid #D0D5DD',
-          paddingTop: '10px',
-          paddingBottom: '10px',
-        }"
-      >
-        <el-table-column min-width="130">
-          <template #header>
-            <p class="body-small-semi-bold neutral-500">Ảnh đại diện</p>
-          </template>
-          <template #default="scope">
-            <el-image :src="scope.row?.thumbnail || ''" style="width: 100px" fit="cover" />
-          </template>
-        </el-table-column>
-        <el-table-column min-width="220">
-          <template #header>
-            <p class="body-small-semi-bold neutral-500">Tiêu đề</p>
-          </template>
-          <template #default="scope">
-            <el-link type="primary" underline="never" @click="onDirectPostDetail(scope.row.id)">{{
-              scope.row.title
-            }}</el-link>
-          </template>
-        </el-table-column>
-        <el-table-column min-width="160">
-          <template #header>
-            <p class="body-small-semi-bold neutral-500">Giá (/1 phòng)</p>
-          </template>
-          <template #default="scope">
-            <p class="body-small-regular neutral-600">
-              {{ formatPrice(scope.row.price) }}
-            </p>
-          </template>
-        </el-table-column>
-        <el-table-column min-width="220">
-          <template #header>
-            <p class="body-small-semi-bold neutral-500">Tỉnh/Thành phố</p>
-          </template>
-          <template #default="scope">
-            <p class="body-small-regular neutral-600">
-              {{ scope.row.provinceName }}
-            </p>
-          </template>
-        </el-table-column>
-        <el-table-column min-width="220">
-          <template #header>
-            <p class="body-small-semi-bold neutral-500">Xã/Quận</p>
-          </template>
-          <template #default="scope">
-            <p class="body-small-regular neutral-600">
-              {{ scope.row.wardName }}
-            </p>
-          </template>
-        </el-table-column>
-        <el-table-column min-width="160" align="center" fixed="right">
-          <template #default="scope">
-            <div v-if="filter.status === 2">
-              <el-button type="primary" @click="onPostAccept(scope.row)">
-                <el-icon><CircleCheck /></el-icon>
-                <span>Duyệt</span>
-              </el-button>
+                <div>
+                    <el-button type="primary" @click="onDirectProjectCreate"
+                        >Tạo Tin đăng</el-button
+                    >
+                </div>
             </div>
-          </template>
-        </el-table-column>
-      </el-table>
+        </div>
 
-      <!-- <Pagination
+        <div class="el-table-outer">
+            <el-table
+                v-loading="ui.isLoading"
+                :data="tableData"
+                :header-cell-style="{
+                    background: '#F9FAFB',
+                    borderBottom: '1px solid #D0D5DD',
+                    paddingTop: '10px',
+                    paddingBottom: '10px',
+                }"
+            >
+                <el-table-column min-width="130">
+                    <template #header>
+                        <p class="body-small-semi-bold neutral-500">Ảnh đại diện</p>
+                    </template>
+                    <template #default="scope">
+                        <el-image
+                            :src="scope.row?.thumbnail || ''"
+                            style="width: 100px"
+                            fit="cover"
+                        />
+                    </template>
+                </el-table-column>
+                <el-table-column min-width="220">
+                    <template #header>
+                        <p class="body-small-semi-bold neutral-500">Tiêu đề</p>
+                    </template>
+                    <template #default="scope">
+                        <el-link
+                            type="primary"
+                            underline="never"
+                            @click="onDirectPostDetail(scope.row.id)"
+                            >{{ scope.row.title }}</el-link
+                        >
+                    </template>
+                </el-table-column>
+                <el-table-column min-width="160">
+                    <template #header>
+                        <p class="body-small-semi-bold neutral-500">Giá (/1 phòng)</p>
+                    </template>
+                    <template #default="scope">
+                        <p class="body-small-regular neutral-600">
+                            {{ formatPrice(scope.row.price) }}
+                        </p>
+                    </template>
+                </el-table-column>
+                <el-table-column min-width="220">
+                    <template #header>
+                        <p class="body-small-semi-bold neutral-500">Tỉnh/Thành phố</p>
+                    </template>
+                    <template #default="scope">
+                        <p class="body-small-regular neutral-600">
+                            {{ scope.row.provinceName }}
+                        </p>
+                    </template>
+                </el-table-column>
+                <el-table-column min-width="220">
+                    <template #header>
+                        <p class="body-small-semi-bold neutral-500">Xã/Quận</p>
+                    </template>
+                    <template #default="scope">
+                        <p class="body-small-regular neutral-600">
+                            {{ scope.row.wardName }}
+                        </p>
+                    </template>
+                </el-table-column>
+                <el-table-column min-width="160" align="center" fixed="right">
+                    <template #default="scope">
+                        <div v-if="filter.status === 2">
+                            <el-button type="primary" @click="onPostAccept(scope.row)">
+                                <el-icon><CircleCheck /></el-icon>
+                                <span>Duyệt</span>
+                            </el-button>
+                        </div>
+                        <div>
+                            <el-button type="primary" @click="onPostCopyLink(scope.row)">
+                                <span>Copy Link</span>
+                            </el-button>
+                        </div>
+                    </template>
+                </el-table-column>
+            </el-table>
+
+            <!-- <Pagination
                 v-show="tableData.length !== 0"
                 ref="paginationRef"
                 :pageSizes="[10, 20, 30, 50]"
                 @changePageSize="handleSizeChange"
                 @changeCurrentPage="handleCurrentChange"
             /> -->
-    </div>
+        </div>
 
-    <!-- =================== -->
-    <PopupAccept ref="dialogRef" @onSuccess="fetchPosts" />
-    <!-- =================== -->
-  </HaSection>
+        <!-- =================== -->
+        <PopupAccept ref="dialogRef" @onSuccess="fetchPosts" />
+        <!-- =================== -->
+    </HaSection>
 </template>
 
 
@@ -131,20 +145,21 @@ const { setStatus, setTypeStatus } = usePage()
 import { useUI } from '@/mixins/globalMixin'
 const { formatPrice } = useUI()
 import PopupAccept from '../components/PopupAccept.vue'
+import { ElMessage } from 'element-plus'
 
 const router = useRouter()
 
 const tableData = ref<any>([])
 
 const ui = reactive({
-  isLoading: false,
+    isLoading: false,
 })
 
 const statusOptions = [
-  { label: 'Hoạt động', value: 1 },
-  { label: 'Chờ duyệt', value: 2 },
-  { label: 'Hết hạn', value: 3 },
-  { label: 'Bị từ chối', value: 4 },
+    { label: 'Hoạt động', value: 1 },
+    { label: 'Chờ duyệt', value: 2 },
+    { label: 'Hết hạn', value: 3 },
+    { label: 'Bị từ chối', value: 4 },
 ]
 
 // const filter = reactive({
@@ -153,14 +168,14 @@ const statusOptions = [
 //     keyword: '',
 // })
 const filter: PostFilter = {
-  page: 1,
-  provinceId: 28,
-  status: 1,
-  // limit: 10,
-  // keyword: '',
-  // wardId: null,
-  // price: null,
-  // areaId: null,
+    page: 1,
+    provinceId: 28,
+    status: 1,
+    // limit: 10,
+    // keyword: '',
+    // wardId: null,
+    // price: null,
+    // areaId: null,
 }
 
 // const handleSizeChange = (value: any) => {
@@ -176,29 +191,29 @@ const filter: PostFilter = {
 // }
 
 const fetchPosts = async () => {
-  try {
-    ui.isLoading = true
-    const { res } = await getPosts(filter)
-    tableData.value = res.data?.phongTroList // chỉ lấy danh sách
+    try {
+        ui.isLoading = true
+        const { res } = await getPosts(filter)
+        tableData.value = res.data?.phongTroList // chỉ lấy danh sách
 
-    console.log('tableData :', res)
-  } catch (e) {
-    console.error(e)
-  } finally {
-    ui.isLoading = false
-  }
+        console.log('tableData :', res)
+    } catch (e) {
+        console.error(e)
+    } finally {
+        ui.isLoading = false
+    }
 }
 
 const onSearch = () => {
-  fetchPosts()
+    fetchPosts()
 }
 
 onMounted(() => {
-  fetchPosts()
+    fetchPosts()
 })
 
 const onDirectProjectCreate = () => {
-  router.push({ name: 'PostCreate' })
+    router.push({ name: 'PostCreate' })
 }
 
 // const onDirectProjectEdit = (value: number) => {
@@ -212,19 +227,26 @@ const onDirectProjectCreate = () => {
 
 const dialogRef = ref<any>(null)
 const onPostAccept = (row: any) => {
-  dialogRef.value?.open(row)
+    dialogRef.value?.open(row)
 }
 
 const onDirectPostDetail = (id: any) => {
-  router.push({ name: 'PostDetail', query: { id: id } })
+    router.push({ name: 'PostDetail', query: { id: id } })
+}
+
+const onPostCopyLink = (row: any) => {
+    ElMessage({
+        message: `Đã sao chép link: ${window.location.origin}/posts/token=abc123&id=${row.id}`,
+        type: 'success',
+    })
 }
 </script>
 
 
 <style lang="scss" scoped>
 .el-popover_item {
-  div {
-    margin: 4px 0;
-  }
+    div {
+        margin: 4px 0;
+    }
 }
 </style>
